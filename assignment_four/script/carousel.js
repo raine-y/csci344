abbe50d5b1882295f8db27ea57169464d547b86e
Raine <rainehwalker@proton.me>
2025-10-17 17:00:21 -0400

    worst autoslideshow youve ever seen

--- assignment_four/script/carousel.js
+++ assignment_four/script/carousel.js
@@ -1,1 +1,14 @@
-// 5 Image Carousel
+let carouselIndex = 0;
+carouselHandler();
+
+function carouselHandler() {
+    let i;
+    let carouselItem = document.getElementsByClassName("carouselItem");
+    for (i = 0; i < carouselItem.length; i++) {
+        carouselItem[i].style.display = "none";
+    }
+    carouselIndex++;
+    if (carouselIndex > carouselItem.length) { carouselIndex = 1 }
+    carouselItem[carouselIndex - 1].style.display = "block";
+    setTimeout(carouselHandler, 2000); // Change image every
+}
