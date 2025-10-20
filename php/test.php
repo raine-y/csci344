96218ac00d02102d68d1ba4aa16eddce38519317
Raine <rainehwalker@proton.me>
2025-10-20 13:53:21 -0400

    okay

--- php/test.php
+++ php/test.php
@@ -21,6 +21,31 @@
     }
     test();
     $GLOBALS[""] = "";
+
+    for ($i = 0; $i < 10; $i++) {
+        if ($i == 5)
+            continue;
+        echo $i;
+    }
+
+    for ($i = 0; $i <= 1000; $i+=100) {
+        if ($i == 500)
+            continue;
+        echo $i;
+    }
+
+    $members = array("John" => "35", "Mary" => "36", "Bob" => "37");
+    foreach ($members as $x) {
+        echo $x;
+    }
+
+    function sumMyNumbers(...$x) { // ... == unknown amount of arguments
+        $sum = 0;
+        foreach ($x as $num) {
+            $sum += $num;
+        }
+        return $sum;
+    }
     ?>
 </body>
 
