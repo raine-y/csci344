080f59f6f29ad4939bd989da61361bf1f1b32954
Raine <rainehwalker@proton.me>
2025-10-23 17:26:31 -0400

    carousel

--- assignment_four/script/carousel.js
+++ assignment_four/script/carousel.js
@@ -3,7 +3,7 @@ carouselHandler();
 
 function carouselHandler() {
     let i;
-    let carouselItem = document.getElementsByClassName("carouselItem");
+    let carouselItem = document.getElementsByClassName("carouselItem"); //array of all items(?)
     for (i = 0; i < carouselItem.length; i++) {
         carouselItem[i].style.display = "none";
     }
@@ -12,5 +12,7 @@ function carouselHandler() {
         carouselIndex = 1; 
     }
     carouselItem[carouselIndex - 1].style.display = "block";
-    setTimeout(carouselHandler, 2000);
+    // carouselItem[carouselIndex].style.display = "block";
+    // carouselItem[carouselIndex - 1].classList.add("fadeIn");
+    setTimeout(carouselHandler, 4000);
 }
