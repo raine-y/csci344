8a005ed646b5f491e3028dac2ad9f170ab06968e
Raine <rainehwalker@proton.me>
2025-11-19 15:05:43 -0500

    dogshit styling

--- project_two/php/loginResponse.php
+++ project_two/php/loginResponse.php
@@ -1,36 +1,35 @@
-<?php
-session_start();
-include "connectToDatabase.php";
-$query = "SELECT * FROM Users
-WHERE login_username = '$_POST[username]'";
-$rowsWithMatchingLoginName = mysqli_query($db, $query);
-$numRecords = mysqli_num_rows($rowsWithMatchingLoginName);
-if ($numRecords == 0) {
-    echo "<h3>Login name not found. Please try again.</h3>";
-}
-if ($numRecords == 1) {
-    $row = mysqli_fetch_array($rowsWithMatchingLoginName, MYSQLI_ASSOC);
-    if ($_POST['loginPassword'] == $row['login_password']) {
-        $_SESSION['customer_id'] = $row['customer_id'];
-        $_SESSION['salutation'] = $row['salutation'];
-        $_SESSION['customer_first_name'] = $row['customer_first_name'];
-        $_SESSION['customer_middle_initial'] =
-            $row['customer_middle_initial'];
-        $_SESSION['customer_last_name'] = $row['customer_last_name'];
-        $productID = $_SESSION['purchasePending'];
-        if ($productID != "") {
-            unset($_SESSION['purchasePending']);
-            $destination =
-                "../pages/shoppingCart.php?productID=$productID";
-            $goto = "Location: $destination";
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    include "connectToDatabase.php";
+    $query = "SELECT * FROM Users WHERE username = '$_POST[login_username]'";
+    $rowsWithMatchingLoginName = mysqli_query($db, $query);
+    $numRecords = mysqli_num_rows($rowsWithMatchingLoginName);
+    if ($numRecords == 0) {
+        echo "<h3>Login name not found. Please try again.</h3>";
+    }
+    if ($numRecords == 1) {
+        $row = mysqli_fetch_array($rowsWithMatchingLoginName, MYSQLI_ASSOC);
+        if ($_POST['password'] == $row['user_login']) {
+            echo "<h3>Login successful. Welcome, " . $row['username'] . "!</h3>";
         } else {
-            $destination = getenv('HTTP_REFERER');
-            $goto = "Location: " . $destination;
+            echo "<h3>Incorrect password. Please try again.</h3>";
         }
         header($goto);
     } else {
         header("Location: ../pages/loginForm.php?retrying=true");
     }
-}
-mysqli_close($db);
-?>
+    mysqli_close($db);
+    ?>
+</body>
+
+</html>
