480af85bb8d9955a0ed3a3a2d8cae88e7e321daf
Raine <rainehwalker@proton.me>
2025-11-18 19:43:54 -0500

    all

--- project_two/php/registrationResponse.php
+++ project_two/php/registrationResponse.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href=" ../css/registration.css" />
+    <link rel="stylesheet" type="text/css" href="../css/registration.css" />
     <?php include "connectToDatabase.php"; ?>
     <title>Registration</title>
 </head>
