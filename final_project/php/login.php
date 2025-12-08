e68236b1ef962900ca36ee50c80ab751982b4d1a
Raine <rainehwalker@proton.me>
2025-12-07 21:18:25 -0500

    login stylin

--- final_project/php/login.php
+++ final_project/php/login.php
@@ -3,23 +3,31 @@
 
 <head>
     <meta charset="UTF-8">
+    <link rel="stylesheet" href="../css/fonts.css" />
+    <link rel="stylesheet" href="../css/login.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 </head>
 
 <body>
-    <main>
-        <article class="Login">
-            <h4>Login Form</h4>
+    <header
+        <main>
+        <a id="homeBtn" href="../index.php">Return Home</a>
+        <article class="login">
+            <h4>Login</h4>
             <form method="post" action="loginResponse.php">
                 <label for="login_username">Username:</label>
                 <input type="text" id="login_username" name="login_username" required><br><br>
                 <label for="password">Password:</label>
                 <input type="password" id="password" name="password" required><br><br>
-                <input type="submit" value="Login">
+                <div id="inputs">
+                    <input type="submit" value="Login">
+                    <p>- or -</p>
+                    <a href="registration.php">Register</a>
+                </div>
             </form>
         </article>
-    </main>
+        </main>
 </body>
 
 </html>
